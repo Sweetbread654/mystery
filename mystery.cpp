34cce837f4d0cbc399df294d9a05da3cf32b96dd
Sweetbread654 <Sweetbread654@users.noreply.github.com>
2019-02-26 12:02:43 -0400

    mystery

--- mystery.cpp
+++ mystery.cpp
@@ -14,14 +14,52 @@ void mystery1(auto& Data)
 {
   cout<<endl<<"Mystery 1"<<endl<<"---------------------"<<endl;
 
-  for ( int i = 0 ; i < Data.size( ) ; i++)
+  for (int i = 0 ; i<Data.size() ; i++)
   {
-    for ( int j = 0 ; j < i ; j++)
-	if ( Data[ i ] < Data[ j ] )
-	    swap( Data[ i ] , Data[ j ] );
+    for ( int j=0 ; j<i ; j++)
+    {
+		if ( Data[i]<Data[j] )
+		{
+			swap(Data[i] , Data[j]);
+		}
+	}
+		print(Data);
+	}//end outer for (this brace is needed to include the print statement)
 
-    print(Data);
-  }//end outer for (this brace is needed to include the print statement)
+}
+
+void mystery2(auto& Data)
+{
+	cout<<endl<<"Mystery 2"<<endl<<"---------------------"<<endl;
+	for ( int i=0 ; i < Data.size() ; i++)
+	{
+		for ( int j=0 ; j<(Data.size()−1) ; j++)
+		{
+			if ( Data[j+1] < Data[j] )
+			{
+				swap ( Data [j+1] , Data [j] ) ;
+			}
+		}
+		print(Data);	
+	}
+
+}
+
+void mystery3(auto& Data)
+{
+	
+	cout<<endl<<"Mystery 3"<<endl<<"---------------------"<<endl;
+	for ( int i=0 ; i<Data.size() ; i++)
+	{
+		for ( int j=i ; j<Data.size() ; j++)
+		{
+			if ( Data[j]<Data[i] )
+		
+			swap( Data[i] , Data[j] ) ;
+			
+		}
+		print(Data);
+	}
 
 }
 
